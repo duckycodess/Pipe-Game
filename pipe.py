b96d13877f09e6cde7a0265b79a4bd89b13b6fbb
Tristan Jovan Noval <138286670+duckycodess@users.noreply.github.com>
2024-05-17 00:24:47 +0800

    Completed the code

--- pipe.py
+++ pipe.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 from dataclasses import dataclass
-from random import Random, getrandbits
+from random import Random, getrandbits, randint
 from typing import Final
 
 import pyxel
@@ -12,10 +12,17 @@ import pipelib as pl
 TITLE: Final[str] = "Pipe Dream"
 FPS: Final[int] = 25
 
-
 @dataclass
 class CellState:
     ... # TODO
+    north: bool = False
+    south: bool = False
+    east: bool = False
+    west: bool = False
+    base: bool = False
+    active: bool = False
+    has_been_rotated: bool = False
+    letter: str = ''
 
 
 class PipeGame(pg.PyxelGrid[CellState]):
@@ -23,6 +30,10 @@ class PipeGame(pg.PyxelGrid[CellState]):
         self.settings = settings
         self.seed = seed
         self.rand = Random(seed)
+        self.paths = pl.generate_paths(settings, self.rand)
+        self.cells: set[tuple[int, int]] = set()
+        self.start_cells: dict[tuple[int, int], list[tuple[int, int]]] = {}
+        self.completed = False
 
         ... # TODO
 
@@ -34,24 +45,136 @@ class PipeGame(pg.PyxelGrid[CellState]):
         pyxel.rseed(self.seed)
 
         ... # TODO
+        letter = 'A'
+        for path in self.paths:
+
+            self.start_cells[path[0]] = path
+
+            for (i, j), (ni, nj) in zip(path[:], path[1:]):
+                if (i, j) not in self._cell_state:
+                    self._cell_state[(i, j)] = CellState()
+                    self.cells.add((i, j))
+                if (ni, nj) not in self._cell_state:
+                    self._cell_state[(ni, nj)] = CellState()
+                    self.cells.add((ni, nj))
+                
+                self.connect_cells((i, j), (ni, nj))
+
+            self._cell_state[path[0]].base = True
+            self._cell_state[path[0]].letter = letter
+            self._cell_state[path[-1]].base = True
+            self._cell_state[path[-1]].letter = letter
+
+            letter = chr(ord(letter)+1)
+
+        
+        for r in range(self.r):
+            for c in range(self.c):
+                if (r, c) not in self.cells:
+                    n = randint(1,2)
+                    if n == 1:
+                        self._cell_state[(r, c)] = CellState(north=True, west= True)
+                    else:
+                        self._cell_state[(r, c)] = CellState(north=True, south=True)
+                randomizer = randint(0, 3)
+                for _ in range(randomizer):
+                    if not self[(r,c)].base:
+                        self.rotate_pipe(r, c)
+
+    def connect_cells(self, cell1: tuple[int, int], cell2: tuple[int, int]):
+            i1, j1 = cell1
+            i2, j2 = cell2
+            if i1 == i2:
+                if j1 < j2:
+                    self._cell_state[cell1].east = True
+                    self._cell_state[cell2].west = True
+                else:
+                    self._cell_state[cell1].west = True
+                    self._cell_state[cell2].east = True
+            elif j1 == j2:
+                if i1 < i2:
+                    self._cell_state[cell1].south = True
+                    self._cell_state[cell2].north = True
+                else:
+                    self._cell_state[cell1].north = True
+                    self._cell_state[cell2].south = True
+    
+    def rotate_pipe(self, i: int, j: int):
+        cell = self._cell_state[(i, j)]
+        cell.north, cell.east, cell.south, cell.west = cell.west, cell.north, cell.east, cell.south
+        cell.has_been_rotated = True
+        pyxel.play(3, pl.SFX_PRESS_SUCCESS)
+        self.check_active_paths()
+
+    def check_active_paths(self) -> None:
+        for path in self.paths:
+            for i, j in path:
+                self[i, j].active = False
+        
+        for path in self.paths:
+            if self.is_path_active(path):
+                for i, j in path:
+                    self[i, j].active = True
+                pyxel.play(3, pl.SFX_INCREASED_ACTIVE_PIPES)
+        
+        if all(self.is_path_active(path) for path in self.paths):
+            pyxel.play(3, pl.SFX_WIN)
+            self.completed = True
+
+    def is_path_active(self, path: list[tuple[int, int]]) -> bool:
+        for k, (i, j) in enumerate(path):
+            if k > 0:
+                prev_i, prev_j = path[k-1]
+                cell = self[i, j]
+                prev_cell = self[prev_i, prev_j]
+                if prev_i < i and not (prev_cell.south and cell.north):
+                    return False
+                if prev_i > i and not (prev_cell.north and cell.south):
+                    return False
+                if prev_j < j and not (prev_cell.east and cell.west):
+                    return False
+                if prev_j > j and not (prev_cell.west and cell.east):
+                    return False
+        return True
 
     def update(self) -> None:
-
+        if self.completed:
+            return
         # TODO remove this debugging message
-        print("The mouse is at cell", self.mouse_cell())
+        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+            mi, mj = self.mouse_cell()
+            if self.in_bounds(mi, mj):
+                if (mi, mj) in self._cell_state and not self._cell_state[(mi, mj)].base:
+                    self.rotate_pipe(mi, mj)
+                elif self._cell_state[(mi, mj)].base:
+                    pyxel.play(1, pl.SFX_PRESS_FAIL)
 
         ... # TODO
 
     def draw_cell(self, i: int, j: int, x: int, y: int) -> None:
+        cell = self._cell_state.get((i, j), CellState())
+        pl.draw_pipe(
+            x, y,
+            active=cell.active,
+            north=cell.north,
+            south=cell.south,
+            east=cell.east,
+            west=cell.west,
+            base=cell.base,
+        )
 
+        if cell.letter != '':
+            pyxel.text(x+6, y+5, cell.letter, pl.COLOR_TEXT_INACTIVE)
+            if cell.active:
+                pyxel.text(x+6, y+5, cell.letter, pl.COLOR_TEXT_ACTIVE)
         ... # TODO
 
     def pre_draw_grid(self) -> None:
-        
         ... # TODO
 
     def post_draw_grid(self) -> None:
-        
+        if self.completed:
+            pyxel.text(pyxel.width//2 - 10, pyxel.height//2, 'You Win!', 14)
         ... # TODO
 
 
